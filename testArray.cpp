3128550edd94e15a72f56e869c64a3437d4b6b94
kongsicong <1390159970@qq.com>
2016-03-31 14:34:25 +0800

    the test file for the array

---
+++ testArray.cpp
@@ -0,0 +1,8 @@
+#include "array/Solution.h"
+#include "testArray.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void testArray() {
+
+}
---
+++ testArray.h
@@ -0,0 +1,4 @@
+#ifndef _TEST_ARRAY_H_
+#define _TEST_ARRAYH_
+void testArray();
+#endif //_TEST_ARRAY_H_
