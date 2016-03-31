de86e992ec2f8f08cc4fb208bd35c1375c4b2609
kongsicong <1390159970@qq.com>
2016-03-31 14:33:11 +0800

    modify test.cpp to test modules independently

--- test.cpp
+++ test.cpp
@@ -1,7 +1,20 @@
-#include "list/Solution.h"
+#include "test.h"
 #include <stdlib.h>
-int main() {
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+int main(int argc, char* argv[]) {
+	char option[20];
+	printf("please input the option:\n");
+	scanf("%s", option);
+	while (strcmp(option, "exit")) {
+		if (!strcmp(option, "list")) testList();
+		else if (!strcmp(option, "array")) testArray();
 
 
+		printf("please input the option:\n");
+		scanf("%s", option);
+
+	}
 	return 0;
 }
